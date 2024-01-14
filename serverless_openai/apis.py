2dca41c281a0d97f58cd029c0a7571ea378f24e8
Jayr <jayrcastro.py@gmail.com>
2024-01-14 18:38:57 +0800

    added embeddings and scraping bee

--- serverless_openai/apis.py
+++ serverless_openai/apis.py
@@ -1,15 +1,32 @@
-import requests, json
 from typing import Optional, List, Union
 from pydantic import BaseModel
 from serverless_openai.helpers import *
+from bs4 import BeautifulSoup
+import re
 
 class OpenAIAPI(BaseModel):
     api_key: str
     org_key: Optional[str] = None
+    headers: Optional[dict] = None
     text_models: List[str] = TextCompletionModels
     imagecreation_models: List[str] = ImageCreationModels
-    completion_url: str = "https://api.openai.com/v1/chat/completions"
-    imagecreation_url: str = "https://api.openai.com/v1/images/generations"
+    completion_url: HttpUrlString = "https://api.openai.com/v1/chat/completions"
+    imagecreation_url: HttpUrlString = "https://api.openai.com/v1/images/generations"
+    embeddings_url: HttpUrlString = "https://api.openai.com/v1/embeddings"
+    
+
+    @validator('headers', always=True)
+    def check_url(cls, v, values):
+        if not v:
+            h: dict = {
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {values['api_key']}"
+            }
+            if values['org_key']:
+                h["OpenAI-Organization"] = values['org_key']
+            return h
+        return v
+
 
     class Config:  
         use_enum_values = True
@@ -34,14 +51,8 @@ class OpenAIAPI(BaseModel):
             stop: Optional[list] = None,
             stream: Optional[bool] = False,
             top_p: Optional[int] = 1
-        ):
+        ) -> str:
         messages = messages.model_dump()['messages']
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {self.api_key}"
-        }
-        if self.org_key:
-            headers["OpenAI-Organization"] = self.org_key
         
         data = {
             "model": model,
@@ -62,8 +73,7 @@ class OpenAIAPI(BaseModel):
         }
         for _ in range(tries):
             try:
-                res = requests.post(self.completion_url, headers=headers, json=data, timeout=timeout).json()
-                # print(res)
+                res = requests.post(self.completion_url, headers=self.headers, json=data, timeout=timeout).json()
                 if 'choices' in res:
                     message = res['choices'][0]['message']
                     return message['content']
@@ -92,20 +102,10 @@ class OpenAIAPI(BaseModel):
             seed: Optional[int] = None,
             stop: Optional[list] = None,
             stream: Optional[bool] = False,
-            top_p: Optional[int] = 1,
-            
-        ):
+            top_p: Optional[int] = 1,  
+        ) -> dict:
         messages = messages.model_dump()['messages']
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {self.api_key}"
-        }
-        if self.org_key:
-            headers["OpenAI-Organization"] = self.org_key
-        # Number between -2.0 and 2.0.
-        # Positive values penalize new tokens based on whether they appear
-        # in the text so far, increasing the model's
-        # likelihood to talk about new topics.
+
         data = {
             "model": model,
             "messages": messages,
@@ -127,8 +127,7 @@ class OpenAIAPI(BaseModel):
         }
         for _ in range(5):
             try:
-                results = requests.post(self.completion_url, headers=headers, json=data, timeout=timeout).json()
-                print(results)
+                results = requests.post(self.completion_url, headers=self.headers, json=data, timeout=timeout).json()
                 if 'choices' in results:
                     res = results['choices'][0]['message']
                     res_json = json.loads(res['tool_calls'][0]['function']['arguments'], strict=False)
@@ -146,21 +145,15 @@ class OpenAIAPI(BaseModel):
             n: int = 1,
             size: str = "1024x1024",
             timeout: int = 500
-        ):
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {self.api_key}"
-        }
-        if self.org_key:
-            headers["OpenAI-Organization"] = self.org_key
-        
+        ) -> dict:
+
         data = {
             "model": model,
             "prompt": prompt,
             "n": n,
             "size": size,
         }
-        results = requests.post(self.imagecreation_url, headers=headers, json=data, timeout=timeout).json()
+        results = requests.post(self.imagecreation_url, headers=self.headers, json=data, timeout=timeout).json()
         return results
     
     def vision(
@@ -171,13 +164,7 @@ class OpenAIAPI(BaseModel):
             timeout: int = 500,
             temperature: Optional[float] = 1,
             max_tokens: Optional[int] = 1024,
-        ):
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {self.api_key}"
-        }
-        if self.org_key:
-            headers["OpenAI-Organization"] = self.org_key
+        ) -> str:
 
         newm = [
             {
@@ -190,7 +177,7 @@ class OpenAIAPI(BaseModel):
                     {
                         "type": "image_url",
                         "image_url": {
-                            "url": messages.image_url if messages.image_url else messages.image_path,
+                            "url": messages.image,
                             "detail": "high"
                         }
                     }
@@ -205,11 +192,112 @@ class OpenAIAPI(BaseModel):
         }
         for _ in range(tries):
             try:
-                res = requests.post(self.completion_url, headers=headers, json=data, timeout=timeout).json()
-                print(res)
+                res = requests.post(self.completion_url, headers=self.headers, json=data, timeout=timeout).json()
                 if 'choices' in res:
                     message = res['choices'][0]['message']
                     return message['content']
             except Exception as e:
                 print("ERROR:", e)
         return False
+
+    def embeddings(
+            self,
+            prompt: EmbeddingPrompts,
+            model: EmbeddingModels = EmbeddingModels.ada2,
+            tries: int = 5,
+            timeout: int = 500,
+        ) -> list:
+        data = {
+            "model": model,
+            "input": prompt,
+        }
+        for _ in range(tries):
+            try:
+                res = requests.post(self.embeddings_url, headers=self.headers, json=data, timeout=timeout).json()
+                if 'data' in res:
+                    return [x['embedding'] for x in res['data']]
+            except Exception as e:
+                print("ERROR:", e)
+        return False
+    
+    def cosine_similarity(
+            self, 
+            sim: Similarity,
+            data_list: List[str],
+            get_scores: bool = False
+        ) -> list:
+        vector = np.array(sim.vector)
+        matrix = np.array(sim.matrix)
+        scores = (np.sum(vector*matrix,axis=1) / ( np.sqrt(np.sum(matrix**2,axis=1)) * np.sqrt(np.sum(vector**2)) ) )
+        if get_scores:
+            return scores
+        result_list = self.get_similarity_result(scores, data_list)
+        return result_list
+    
+    def get_similarity_result(
+            self,
+            scores: list, 
+            all_data: list, 
+            topn: int = 5
+        ) -> list:
+        idx = (-scores).argsort()
+        result_list = []
+        for ix in idx[:topn]:
+            p = all_data[ix]
+            scr = scores[ix]
+            result_list.append(p)
+        return result_list
+
+class ScrapingBeeAPI(BaseModel):
+    api_key: str
+    scrape_url: HttpUrlString = "https://app.scrapingbee.com/api/v1"
+
+    def scrape(
+            self,
+            url: str,
+            get_links: bool = False,
+            block_resources: bool = True,
+            render_js: bool = True,
+            wait_browser: str = 'load',
+            premium_proxy: bool = False,
+            stealth_proxy: bool = True,
+            block_ads: bool = True,
+            js_scenario: dict = {
+                "instructions": [
+                    {"evaluate": 'document.querySelectorAll("style").forEach(function(e){e.remove();});'}
+                ]
+            },
+            extract_rules: str = None
+        ) -> dict:
+
+        params = {
+            'api_key': self.api_key,
+            'url': url,
+            "block_resources": block_resources,
+            'render_js': render_js,
+            'wait_browser': wait_browser,
+            'premium_proxy': premium_proxy,
+            'stealth_proxy': stealth_proxy,
+            'block_ads': block_ads,
+            'block_resources': block_resources,
+            "js_scenario": json.dumps(js_scenario),
+        }
+        if extract_rules:
+            params['extract_rules'] = extract_rules
+
+        resp = requests.get(self.scrape_url, params=params)
+        soup = BeautifulSoup(resp.content, 'html.parser')
+        body = soup.get_text()
+        body = re.sub('\n+', '\n', body).strip()
+        result = {
+            'body': body
+        }
+        if get_links:
+            all_links = []
+            params['extract_rules'] = '''{"all_links":{"selector":"a@href", "type":"list"}}'''
+
+            all_links = requests.get(self.scrape_url, params=params).json()['all_links']
+            all_links = list(dict.fromkeys(all_links))
+            all_links = [x for x in all_links if x is not None]
+            result['links'] = all_links
+        return result
--- serverless_openai/helpers.py
+++ serverless_openai/helpers.py
@@ -1,8 +1,9 @@
 from enum import Enum
-from pydantic import AfterValidator, BaseModel, HttpUrl, validator
+from pydantic import AfterValidator, BaseModel, HttpUrl, validator, TypeAdapter, ValidationError
 from typing import List, Optional, Union
 from typing_extensions import Annotated
-import base64
+import base64, cv2, requests, json, uuid
+import numpy as np
 
 HttpUrlString = Annotated[HttpUrl, AfterValidator(lambda v: str(v))]
 
@@ -40,39 +41,60 @@ class ImageCreationModels(str, ExtendedEnum):
 class VisionModels(str, ExtendedEnum):
     gpt4_vision : str = "gpt-4-vision-preview"
 
+class VisionMessage(BaseModel):
+    text: str
+    role: Roles = Roles.user
+    image: str
+
+    @validator('image', always=True)
+    def check_url(cls, v, values):
+        try:
+            TypeAdapter(HttpUrl).validate_python(v)
+            return v
+        except ValidationError:
+            return encode_image(v)
+
+class EmbeddingModels(str, ExtendedEnum):
+    ada2 : str = "text-embedding-ada-002"
+
+class EmbeddingPrompts(BaseModel):
+    prompt: Union[str, List[str]]
+
+class Similarity(BaseModel):
+    vector: List[List[float]]
+    matrix: List[List[float]]
+
+def download_img(img_url):
+    req = requests.get(
+        url=img_url, 
+        headers={'User-Agent': 'Mozilla/5.0'}
+    )
+    # print()
+    # req = urlopen(req)
+    arr = np.asarray(bytearray(req.content), dtype=np.uint8)
+    img_np = cv2.imdecode(arr, -1) # 'Load it as it is'
+    cv_im = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
+    return cv_im
+    
 def encode_image(image_path):
     with open(image_path, "rb") as image_file:
         base64_image = base64.b64encode(image_file.read()).decode('utf-8')
         return f"data:image/jpeg;base64,{base64_image}"
 
-class VisionMessage(BaseModel):
-    text: str
-    role: Roles = Roles.user
-    image_url: Optional[HttpUrlString] = None
-    image_path: Optional[str] = None
-
-    @validator('image_path', always=True)
-    def check_url(cls, v, values):
-        if v:
-            return encode_image(v)
-        elif not values.get('image_url') and not v:
-            raise ValueError('either a or b is required')
-        return v
-    
-
-
-# class VisionMessages(BaseModel):
-#     messages: List[Message] = []
-
-# class ResultUsage(BaseModel):
-#     prompt_tokens: int
-#     completion_tokens: int
-#     total_tokens: int
-
-# class ResultMessage(BaseModel):
-#     role: str
-#     content: str
-
-# class Results(BaseModel):
-#     usage: ResultUsage
-#     message: ResultMessage
+def crop_image(img_np, dir='images'):
+    curr_h = 0
+    max_h = 4096
+    img_b64_list = []
+    while True:
+        fn = f"{dir}/{uuid.uuid4()}.png"
+        print(curr_h, max_h)
+        crop_img = img_np[curr_h:max_h, :]
+        curr_h = max_h
+        max_h += max_h
+        if not crop_img.shape[0]:
+            break
+        print(crop_img.shape)
+        cv2.imwrite(fn, crop_img)
+        b64_img = encode_image(fn)
+        img_b64_list.append(b64_img)
+    return img_b64_list
