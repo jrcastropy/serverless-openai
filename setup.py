2ede692bef57ffcdafd3d129ee9754c98dcea112
Jayr <jayrcastro.py@gmail.com>
2024-01-15 17:12:36 +0800

    new required packages

--- setup.py
+++ setup.py
@@ -14,7 +14,21 @@ setup(
     long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=['requests', 'opencv-python-headless', 'beautifulsoup4', 'numpy', 'pydantic', 'urllib3==2.1.0'],
+    install_requires=[
+        "annotated-types==0.6.0",
+        "beautifulsoup4==4.12.2",
+        "certifi==2023.11.17",
+        "charset-normalizer==3.3.2",
+        "idna==3.6",
+        "numpy==1.24.4",
+        "opencv-python-headless==4.9.0.80",
+        "pydantic==2.5.3",
+        "pydantic-core==2.14.6",
+        "requests==2.31.0",
+        "soupsieve==2.5",
+        "typing-extensions==4.9.0",
+        "urllib3==2.1.0"
+    ],
     keywords=['serverless', 'openai', 'aws lambda', 'cloud functions', 'openai API'],
     classifiers=[
         "Development Status :: 4 - Beta",
